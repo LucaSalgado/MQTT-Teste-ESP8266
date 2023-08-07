a17f73cd4ca52d562d0fb31924871bb9ca4e0580
Lucas Giovani de Paula Salgado <45075913+LucaSalgado@users.noreply.github.com>
2023-08-07 17:53:09 -0300

    Update MQTT-_sensor-e-LED.ino

--- MQTT-_sensor-e-LED.ino
+++ MQTT-_sensor-e-LED.ino
@@ -4,7 +4,7 @@
 #include <ESP8266WiFi.h> 
 #include <PubSubClient.h>
 
-//defines - mapeamento de pinos do NodeMCU
+// Defines - mapeamento de pinos do NodeMCU
 #define D0    16
 #define D1    5
 #define D2    4
@@ -17,33 +17,32 @@
 #define D9    3
 #define D10   1
 
-//sendores de temperatura e umidade
+// Sensores de temperatura e umidade
 #define DHTTYPE1    DHT11     // DHT 11
 DHT_Unified dht1(D3, DHTTYPE1);
 #define DHTTYPE2    DHT22     // DHT 22 (AM2302)
 DHT_Unified dht2(D5, DHTTYPE2);
 uint32_t delayMS;
 
-//WiFi
-const char* SSID = "Lucas Giovani";                // SSID / nome da rede WiFi que deseja se conectar
+// WiFi
+const char* SSID = "Lucas Giovani";                // SSID - nome da rede WiFi que deseja se conectar
 const char* PASSWORD = "59456797";   // Senha da rede WiFi que deseja se conectar
 WiFiClient wifiClient;                        
  
-//MQTT Server
-const char* BROKER_MQTT = "iot.eclipse.org"; //URL do broker MQTT que se deseja utilizar
+// MQTT Server
+const char* BROKER_MQTT = "iot.eclipse.org"; // URL do broker MQTT que se deseja utilizar
 int BROKER_PORT = 1883;                      // Porta do Broker MQTT
 
-#define ID_MQTT  "ESPSalgado1"            //Informe um ID unico e seu. Caso sejam usados IDs repetidos a ultima conexão irá sobrepor a anterior. 
-#define TOPIC_PUBLISH "SalgadoEnvia"    //Informe um Tópico único. Caso sejam usados tópicos em duplicidade, o último irá eliminar o anterior.
-#define TOPIC_SUBSCRIBE "SalgadoRecebe" //Informe um Tópico único. Caso sejam usados tópicos em duplicidade, o último irá eliminar o anterior.
+#define ID_MQTT  "ESPSalgado1"            // Informe um ID unico e seu. Caso sejam usados IDs repetidos a ultima conexão irá sobrepor a anterior. 
+#define TOPIC_PUBLISH "SalgadoEnvia"    // Informe um Tópico único. Caso sejam usados tópicos em duplicidade, o último irá eliminar o anterior.
+#define TOPIC_SUBSCRIBE "SalgadoRecebe" // Informe um Tópico único. Caso sejam usados tópicos em duplicidade, o último irá eliminar o anterior.
 PubSubClient MQTT(wifiClient);        // Instancia o Cliente MQTT passando o objeto espClient
 
-//Declaração das Funções
-void mantemConexoes();  //Garante que as conexoes com WiFi e MQTT Broker se mantenham ativas
-void conectaWiFi();     //Faz conexão com WiFi
-void conectaMQTT();     //Faz conexão com Broker MQTT
-void enviaPacote();     //
-void recebePacote(char* topic, byte* payload, unsigned int length);
+// Declaração das Funções
+void mantemConexoes();  // Garante que as conexoes com WiFi e MQTT Broker se mantenham ativas
+void conectaWiFi();     // Faz conexão com WiFi
+void conectaMQTT();     // Faz conexão com Broker MQTT
+void recebePacote(char* topic, byte* payload, unsigned int length); // Função de callback para processar os pacotes recebidos
 
 void setup() {
   pinMode(D2, OUTPUT); // definindo o pino D2 para controlar o LED anexado
@@ -76,7 +75,7 @@ void mantemConexoes() {
        conectaMQTT(); 
     }
     
-    conectaWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+    conectaWiFi(); // Se não há conexão com o WiFI, a conexão é refeita
 }
 
 void conectaWiFi() {
@@ -122,7 +121,7 @@ void recebePacote(char* topic, byte* payload, unsigned int length){
 
      String msg;
 
-    //obtem a string do payload recebido
+    // Obtem a string do payload recebido
     for(int i = 0; i < length; i++) 
     {
        char c = (char)payload[i];
@@ -141,8 +140,8 @@ void recebePacote(char* topic, byte* payload, unsigned int length){
     
     sensors_event_t event1;
     dht1.temperature().getEvent(&event1);
-    MQTT.publish(TOPIC_PUBLISH, String(event1.temperature).c_str()); // como a funcão publish só envia Strings,
-    dht1.humidity().getEvent(&event1); //  foi necessário converter as informações vindas do sensor de float para string.
+    MQTT.publish(TOPIC_PUBLISH, String(event1.temperature).c_str()); // Como a funcão publish só envia Strings,
+    dht1.humidity().getEvent(&event1); //  Foi necessário converter as informações vindas do sensor de float para string.
     MQTT.publish(TOPIC_PUBLISH, String(event1.relative_humidity).c_str());
     
   }
